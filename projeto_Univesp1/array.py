93d2629e61e0df75d1a71768cfabe94354d3caab
luizlima898 <luizlimajunior@uol.com.br>
2025-02-27 18:50:45 -0300

    3º Commit

--- projeto_Univesp1/array.py
+++
@@ -1,9 +0,0 @@
-lista =[1, 2, 3, 4, 5, 6, 7] 
-print (lista)
-valor = 6
-if valor in lista:
-   print("There is the number")
-   indice = lista.index(valor)
-   print("The number is set in position:", indice)
-else:
-        print("There is no number") 
