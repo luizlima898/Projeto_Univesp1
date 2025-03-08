8d95d3a403499664a11e0eecbffeaacb1c4d97c0
luizlima898 <luizlimajunior@uol.com.br>
2025-03-08 10:37:25 -0300

    Update estoque.sql

--- estoque.sql
+++ estoque.sql
@@ -1,1 +1,4 @@
-
+CREATE DATABASE IF NOT EXISTS estoque;
+USE estoque;
+CREATE TABLE IF NOT EXISTS estoque( codigo INT AUTO_INCREMENT PRIMARY KEY, nome VARCHAR (200), estoque INT, local VARCHAR (100));
+INSERT INTO estoque (codigo, nome, estoque, local) VALUES (1, 'PORTA DE FERRO ALZ', 50, 'BOX 10');
